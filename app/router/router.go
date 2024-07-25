cda5fabd10e7db17218842bb27353416179b4fe8
Adam Nasrudin <adamnasrudin858@gmail.com>
2024-07-25 13:12:38 +0700

    Add welcome page

--- app/router/router.go
+++ app/router/router.go
@@ -1,9 +1,13 @@
 package router
 
 import (
+	"net/http"
+
+	response_mapper "github.com/adamnasrudin03/go-helpers/response-mapper/v1"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 type Routes struct {
@@ -17,6 +21,14 @@ func NewRoutes() *Routes {
 
 	r.HttpServer.Use(logger.New())
 	r.HttpServer.Use(cors.New())
+	r.HttpServer.Use(recover.New())
+
+	r.HttpServer.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(http.StatusOK).JSON(response_mapper.RenderStruct(http.StatusOK, response_mapper.MultiLanguages{
+			ID: "selamat datang di server ini",
+			EN: "Welcome this server",
+		}))
+	})
 
 	return r
 }
